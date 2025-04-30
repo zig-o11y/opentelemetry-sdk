2fd5f2a5c5e5d57d08893bd426a72f5c74fd0279
Snehasish Basu <snehasishbasupr@gmail.com>
2025-04-30 10:23:58 +0000

    Restoring previous content of basic.zig

--- examples/metrics/basic.zig
+++ examples/metrics/basic.zig
@@ -1,201 +1,53 @@
 const std = @import("std");
-const protobuf = @import("protobuf");
+const sdk = @import("opentelemetry-sdk");
+const MeterProvider = sdk.MeterProvider;
 
-pub fn build(b: *std.Build) void {
-    const target = b.standardTargetOptions(.{});
-    const optimize = b.standardOptimizeOption(.{});
-
-    // Optional filter argument to selectively run benchmarks
-    const benchmark_filter = b.option([]const u8, "benchmark-filter", "Run only benchmarks matching this filter");
-
-    // Dependencies section
-    // Benchmarks lib
-    const benchmarks_dep = b.dependency("zbench", .{
-        .target = target,
-        .optimize = optimize,
+pub fn main() !void {
+    // Use the builtin meter provider
+    const mp = try MeterProvider.default();
+    defer mp.shutdown();
+    const meter = try mp.getMeter(.{
+        .name = "test.company.org/sample",
     });
 
-    // Protobuf code generation from the OpenTelemetry proto files.
-    const protobuf_dep = b.dependency("protobuf", .{
-        .target = target,
-        .optimize = optimize,
+    // Allocate a maximum of 4MiB of memory for the metrics sdk
+    const buf = try std.heap.page_allocator.alloc(u8, 4 << 20);
+    var fba = std.heap.FixedBufferAllocator.init(buf);
+
+    // Declare an in-memory exporter
+    const me = try sdk.MetricExporter.InMemory(fba.allocator(), null, null);
+    defer me.in_memory.deinit();
+
+    // Create an exporter and a a metric reader to aggregate the metrics
+    const mr = try sdk.MetricReader.init(fba.allocator(), me.exporter);
+    defer mr.shutdown();
+
+    // Register the metric reader to the meter provider
+    try mp.addReader(mr);
+
+    const sample_counter = try meter.createCounter(u16, .{
+        .name = "cumulative_sum",
+        .description = "sum of integers",
     });
-
-    const protoc_step = protobuf.RunProtocStep.create(b, protobuf_dep.builder, target, .{
-        // Output directory for the generated zig files
-        .destination_directory = b.path("src"),
-        .source_files = &.{
-            // Add more protobuf definitions as the API grows
-            "proto-src/opentelemetry/proto/common/v1/common.proto",
-            "proto-src/opentelemetry/proto/resource/v1/resource.proto",
-            "proto-src/opentelemetry/proto/metrics/v1/metrics.proto",
-            "proto-src/opentelemetry/proto/trace/v1/trace.proto",
-            "proto-src/opentelemetry/proto/logs/v1/logs.proto",
-        },
-        .include_directories = &.{
-            // Imports in proto files require that the top-level directory
-            // containing the proto files is included
-            "proto-src/",
-        },
-    });
-
-    // Debug protoc generation in all builds
-    protoc_step.verbose = true;
-
-    const gen_proto = b.step("gen-proto", "Generates Zig files from protobuf definitions");
-    gen_proto.dependOn(&protoc_step.step);
-
-    const sdk_lib = b.addStaticLibrary(.{
-        .name = "opentelemetry-sdk",
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("src/sdk.zig"),
-            .target = target,
-            .optimize = optimize,
-            .strip = false,
-            .unwind_tables = .sync,
-        }),
-    });
-
-    sdk_lib.root_module.addImport("protobuf", protobuf_dep.module("protobuf"));
-
-    b.installArtifact(sdk_lib);
-
-    // Providing a way for the user to request running the unit tests.
-    const test_step = b.step("test", "Run unit tests");
-
-    // Creates a step for unit testing the SDK.
-    // This only builds the test executable but does not run it.
-    const sdk_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/sdk.zig"),
-        .target = target,
-        .optimize = optimize,
-        // Allow passing test filter using the build args.
-        .filters = b.args orelse &[0][]const u8{},
-    });
-    sdk_unit_tests.root_module.addImport("protobuf", protobuf_dep.module("protobuf"));
-
-    const run_sdk_unit_tests = b.addRunArtifact(sdk_unit_tests);
-
-    test_step.dependOn(&run_sdk_unit_tests.step);
-
-    // Examples
-    const examples_step = b.step("examples", "Build and run all examples");
-    examples_step.dependOn(&sdk_lib.step);
-
-    // TODO add examples for other signals
-    const metrics_examples = buildExamples(b, "examples/metrics", sdk_lib.root_module) catch |err| {
-        std.debug.print("Error building metrics examples: {}\n", .{err});
-        return;
-    };
-    defer b.allocator.free(metrics_examples);
-    for (metrics_examples) |step| {
-        const run_metrics_example = b.addRunArtifact(step);
-        examples_step.dependOn(&run_metrics_example.step);
+    for (1..5) |d| {
+        try sample_counter.add(@intCast(d), .{ "value", @as(u64, d) });
     }
 
-    // Benchmarks
-    const benchmarks_step = b.step("benchmarks", "Build and run all benchmarks");
-    benchmarks_step.dependOn(&sdk_lib.step);
+    // Collect the metrics from the reader.
+    // This is just an exmple, normally collection would happen in the background,
+    // by using more sophisticated readers.
+    try mr.collect();
 
-    const benchmark_mod = benchmarks_dep.module("zbench");
+    // Print the metrics
+    const stored_metrics = try me.in_memory.fetch(fba.allocator());
+    defer fba.allocator().free(stored_metrics);
 
-    const metrics_benchmarks = buildBenchmarks(
-        b,
-        "benchmarks/metrics",
-        sdk_lib.root_module,
-        benchmark_mod,
-        benchmark_filter,
-    ) catch |err| {
-        std.debug.print("Error building metrics benchmarks: {}\n", .{err});
-        return;
-    };
-    defer b.allocator.free(metrics_benchmarks);
-    for (metrics_benchmarks) |run_step| {
-        benchmarks_step.dependOn(&run_step.step);
-    }
-}
-
-fn buildExamples(b: *std.Build, base_dir: []const u8, otel_mod: *std.Build.Module) ![]*std.Build.Step.Compile {
-    var exes = std.ArrayList(*std.Build.Step.Compile).init(b.allocator);
-    errdefer exes.deinit();
-
-    var ex_dir = try std.fs.cwd().openDir(base_dir, .{ .iterate = true });
-    defer ex_dir.close();
-
-    var ex_dir_iter = ex_dir.iterate();
-    while (try ex_dir_iter.next()) |file| {
-        // Get the file name.
-        // If it doesn't end in 'zig' then ignore.
-        const index = std.mem.lastIndexOfScalar(u8, file.name, '.') orelse continue;
-        if (index == 0) continue; // discard dotfiles
-        if (!std.mem.eql(u8, file.name[index + 1 ..], "zig")) continue;
-
-        const name = file.name[0..index];
-        const example = b.addExecutable(.{
-            .name = name,
-            .root_source_file = b.path(try std.fs.path.join(b.allocator, &.{ base_dir, file.name })),
-            .target = otel_mod.resolved_target.?,
-            // We set the optimization level to ReleaseSafe for examples
-            // because we want to have safety checks, and execute assertions.
-            .optimize = .ReleaseSafe,
-        });
-        example.root_module.addImport("opentelemetry-sdk", otel_mod);
-        try exes.append(example);
-    }
-
-    return exes.toOwnedSlice();
-}
-
-fn buildBenchmarks(
-    b: *std.Build,
-    base_dir: []const u8,
-    otel_mod: *std.Build.Module,
-    benchmark_mod: *std.Build.Module,
-    benchmark_filter: ?[]const u8,
-) ![]*std.Build.Step.Run {
-    var bench_runs = std.ArrayList(*std.Build.Step.Run).init(b.allocator);
-    errdefer bench_runs.deinit();
-
-    var test_dir = try std.fs.cwd().openDir(base_dir, .{ .iterate = true });
-    defer test_dir.close();
-
-    var iter = test_dir.iterate();
-    while (try iter.next()) |file| {
-        // Get the file name.
-        // If it doesn't end in 'zig' then ignore.
-        const index = std.mem.lastIndexOfScalar(u8, file.name, '.') orelse continue;
-        if (index == 0) continue; // discard dotfiles
-        if (!std.mem.eql(u8, file.name[index + 1 ..], "zig")) continue;
-
-        const name = file.name[0..index];
-
-        // Apply benchmark filter if provided
-        if (benchmark_filter) |filter| {
-            if (!std.mem.containsAtLeast(u8, name, 1, filter)) {
-                continue;
-            }
-        }
-
-        const benchmark = b.addTest(.{
-            .name = name,
-            .root_source_file = b.path(try std.fs.path.join(b.allocator, &.{ base_dir, file.name })),
-            .target = otel_mod.resolved_target.?,
-            // We set the optimization level to ReleaseFast for benchmarks
-            // because we want to have the best performance.
-            .optimize = .ReleaseFast,
-        });
-        benchmark.root_module.addImport("opentelemetry-sdk", otel_mod);
-        benchmark.root_module.addImport("benchmark", benchmark_mod);
-
-        const run_step = b.addRunArtifact(benchmark);
-
-        // Forward the filter to the test runner
-        if (benchmark_filter) |filter| {
-            run_step.addArgs(&.{ "--filter", filter });
-        }
-
-        try bench_runs.append(run_step);
-    }
-
-    return bench_runs.toOwnedSlice();
+    // Only 1 instrument collected measurments
+    try std.testing.expectEqual(1, stored_metrics.len);
+    const metric = stored_metrics[0];
+    // Each metric is stored independently because the attribute is different,
+    // beware of cardinality!
+    try std.testing.expectEqual(4, metric.data.int.len);
+    std.debug.assert(metric.data.int[0].attributes != null);
+    std.debug.assert(metric.data.int[0].attributes.?.len == 1);
 }
