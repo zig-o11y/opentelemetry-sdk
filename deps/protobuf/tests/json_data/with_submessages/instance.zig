60239838fbf955a34cc7f396ea6420fd4bce0233
hendriknielaender <hendrik@nielaender.de>
2026-04-23 19:23:29 +0200

    use migrated dep

--- deps/protobuf/tests/json_data/with_submessages/instance.zig
+++
@@ -1,7 +0,0 @@
-const tests = @import("../../generated/tests.pb.zig");
-const WithSubmessages = tests.WithSubmessages;
-const WithEnum = tests.WithEnum;
-
-pub fn get() WithSubmessages {
-    return WithSubmessages{ .with_enum = WithEnum{ .value = .A } };
-}
